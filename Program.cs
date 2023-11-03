cbc5647afc69d9a4158c916c449b2b92cdf30a2b
Mark Rader <mgrader8@gmail.com>
2023-11-03 13:00:38 -0600

    Got the board to generate

--- Program.cs
+++ Program.cs
@@ -1,47 +1,141 @@
-﻿static int[,] InitGameBoard() 
-{ 
-    int[,] board = new int[9,9];
-    foreach (var i in board) {
-        board[i,0] = 0;
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.XPath;
+
+static int[,] InitGameBoard()
+{
+    int[,] board = new int[9, 9];
+    foreach (var i in board)
+    {
+        board[i, 0] = 0;
     }
     return board;
 }
 
-static int[,] GenerateGameBoard(int[,] board) {
+// step through each space and fill it with a valid value
+static int[,] GenerateGameBoard(int[,] board)
+{
     Random rand = new Random();
     int[] numPool = ShuffledRow();
-    for(int i = 0; i < 81; i++) {
-        int currentRow = (int)Math.Floor((double)i/9);
+    for (int i = 0; i < 81; i++)
+    {
+        int currentRow = (int)Math.Floor((double)i / 9);
         int currentCol = i % 9;
-        Console.WriteLine("Current Row -- Col: " + currentRow + " -- " + currentCol);
-        if(board[currentRow,currentCol] == 0) {
-            int temp = rand.Next(1,9);
-            if(!ValidateSpace(board, currentCol, currentRow, temp)) temp = rand.Next(1,9);
-            board[currentRow,currentCol] = temp;
+        //Console.WriteLine("Current Row: {0} Col: {1}", currentRow, currentCol);
+        if (board[currentRow, currentCol] == 0)
+        {
+            int temp = rand.Next(1, 9);
+            // while (!ValidateSpace(board, currentRow, currentCol, temp))
+            while (!ValidateSpace(board, currentCol, currentRow, temp)) temp = rand.Next(1, 9);
+            board[currentRow, currentCol] = temp;
         }
 
     }
     return board;
 }
 
-static bool ValidateSpace(int[,] board, int col, int row, int num) {
-    //int boxRow = (int)Math.Floor((double) row/3);
-    //int boxCol = (int)Math.Floor((double) col/3);
-    for(int i = 0; i < 9; i++) {
-        if(board[i, col] == num) return false;
-        for(int j = 0; j < 9; j++) {
-            if(board[row,i] == num) return false;
+static bool Solve(int[,] board, int row = 0, int col = 0)
+{
+    if (row == 9) return true;
+    else if (col == 9) return Solve(board, row + 1, 0);
+    else if (board[row, col] != 0) return Solve(board, row, col + 1);
+    else
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            if (ValidateSpace(board, row, col, i))
+            {
+                board[row, col] = i;
+                if (Solve(board, row, col + 1)) return true;
+                board[row, col] = 0;
+            }
         }
-        
+        return false;
     }
-    
-    
-    return true;
 }
 
-static int[] ShuffledRow() {
+// Deprecated I don't think I need this one anymore
+// static bool ValidRowHelper(int[,] board, int row)
+// {
+//     HashSet<int> seen = new HashSet<int>();
+//     for (int i = 0; i < 9; i++)
+//     {
+//         seen.Clear();
+//         int current = board[row, i];
+//         if (seen.Add(current)) seen.Add(current);
+//         else return false;
+//     }
+//     return true;
+// }
+
+// Deprecated I don't think I need this one anymore
+// static bool ValidColHelper(int[,] board, int col)
+// {
+//     HashSet<int> seen = new HashSet<int>();
+//     for (int i = 0; i < 9; i++)
+//     {
+//         seen.Clear();
+//         int current = board[i, col];
+//         if (seen.Add(current)) seen.Add(current);
+//         else return false;
+//     }
+//     return true;
+// }
+
+// static bool ValidSquareHelper(int[,] board, int row, int col)
+// {
+//     HashSet<int> seen = new HashSet<int>();
+
+//     return false;
+// }
+
+// might not need this one either, but I will wait to remove it until I get something working
+static bool ValidateGrid(int[,] board)
+{
+    bool result = true;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            int num = board[i, j];
+            result = ValidateSpace(board, i, j, num);
+        }
+    }
+    return result;
+}
+
+static bool ValidateSpace(int[,] board, int row, int col, int num)
+{
+    bool not_in_row = true;
+    bool not_in_col = true;
+    bool not_in_box = true;
+    int box_row = (int)Math.Floor((double)row / 3) * 3;
+    int box_col = (int)Math.Floor((double)col / 3) * 3;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[row, i] == num) not_in_row = false;
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i, col] == num) not_in_col = false;
+    }
+    for (int i = box_row; i < box_row + 3; i++)
+    {
+        for (int j = box_col; j < box_col + 3; j++)
+        {
+            if (board[i, j] == num) not_in_box = false;
+        }
+    }
+    return not_in_row && not_in_col && not_in_box;
+}
+
+// This is pretty but idk if I need it. Creates a randomly shuffled row with LINQ
+// Might be helpful for seeding the board? idk yet though
+static int[] ShuffledRow()
+{
     Random rand = new Random();
-    int[] row = {1,2,3,4,5,6,7,8,9};
+    int[] row = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     row = row.Select(x => (x, rand.Next()))
     .OrderBy(tuple => tuple.Item2)
     .Select(tuple => tuple.Item1)
@@ -49,14 +143,22 @@ static int[] ShuffledRow() {
     return row;
 }
 
-static void PrintBoard(int[,] board) {
-    for (int i = 0; i < 9; i++) {
+static void PrintBoard(int[,] board)
+{
+    for (int i = 0; i < 9; i++)
+    {
         Console.Write("| ");
-        for (int j = 0; j < 9; j++) {
-            Console.Write(board[i,j] + " | ");
+        for (int j = 0; j < 9; j++)
+        {
+            Console.Write(board[i, j] + " | ");
         }
         Console.WriteLine();
     }
 }
 int[,] testBoard = InitGameBoard();
-PrintBoard(GenerateGameBoard(testBoard));
+//PrintBoard(testBoard);
+if (Solve(testBoard))
+{
+    PrintBoard(testBoard);
+}
+//Console.WriteLine(ValidateGrid(testBoard));
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sudoku
 
 This is a repo for me (and anyone who wants to help) to figure out some backtracking for sudoku generation.
+This is just a console app to get going but soon it will be a fully fledged app to rival the NY Times one! I am just obsessed with sudoku and specifically
+the one from the Times because it has the best UI of all the others I have seen.
